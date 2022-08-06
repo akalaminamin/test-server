17a59fbfbc55e473a63a047472c50ea6488184ba
akalamin <akalaminamin91@gmail.com>
2022-08-06 14:25:06 -0700

    update

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('In the name of Allah'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
-
-//visit localhost:3000
